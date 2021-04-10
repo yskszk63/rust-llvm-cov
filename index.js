2a7f6db126bf4de515775ea412e84763c9e0e0d0
yskszk63 <yskszk63@gmail.com>
2021-04-10 15:31:15 +0900

    initial implement

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('it is works!');
+const { spawn } = require('child_process');
+const { mkdirSync } = require('fs');
+
+const CARGO_LLVM_COV_URL = 'https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.1.0-alpha.2/cargo-llvm-cov-x86_64-unknown-linux-musl.tar.gz';
+
+mkdirSync('~/.local/bin', { recursive: true });
+const curl = spawn('curl', ['-sSL', CARGO_LLVM_COV_URL], { stdio: ['ignore', 'pipe', 'inherit'] });
+const tar = spawn('tar', ['zxf', '-', '-C', '~/.local/bin'], { stdio: [curl.stdout, 'inherit', 'inherit'] });
+tar.on('end', _ => {
+    spawn('cargo', ['llvm-cov'], { env: { PATH: `${~/.local/bin:process.env['PATH']}` } });
+});
