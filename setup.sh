a99b53ca0fd3f1230af1110875760a80ac6653ce
yskszk63 <yskszk63@gmail.com>
2021-04-11 00:53:06 +0900

    xfix missing flag

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -e
 CARGO_LLVM_COV_URL='https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.1.0-alpha.2/cargo-llvm-cov-x86_64-unknown-linux-musl.tar.gz'
 
 mkdir -p ~/.local/bin
-curl -sSL $CARGO_LLVM_COV_URL | tar xf - -C ~/.local/bin
+curl -sSL $CARGO_LLVM_COV_URL | tar zxf - -C ~/.local/bin
 cargo install cargo-binutils
 cargo install rustfilt
 echo ~/.local/bin >> $GITHUB_PATH
