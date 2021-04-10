648ab373b0bd77a48dbcd5785ad634da8e4c319f
yskszk63 <yskszk63@gmail.com>
2021-04-11 00:49:12 +0900

    rm setup.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { setFailed } = require('@actions/core');
 const { exec } = require('@actions/exec');
 
-exec('cargo', ['llvm-cov'], {
-    env: {
-        PATH: `~/.local/bin:${process.env['PATH']}`,
-    }
-}).catch(setFailed);
+(async () => {
+    await exec("./setup.sh");
+    await exec('cargo', ['llvm-cov'], {
+        env: {
+            PATH: `~/.local/bin:${process.env['PATH']}`,
+        }
+    })
+})().catch(setFailed);;
--- setup.js
+++
@@ -1,4 +0,0 @@
-const { setFailed } = require('@actions/core');
-const { exec } = require('@actions/exec');
-
-exec("./setup.sh").catch(setFailed);
