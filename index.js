2bb91e6b13eabf3d4c9441bd0a1c460ba27813bf
yskszk63 <yskszk63@gmail.com>
2021-04-10 15:40:16 +0900

    handling exit code

--- index.js
+++ index.js
@@ -6,7 +6,23 @@ const CARGO_LLVM_COV_URL = 'https://github.com/taiki-e/cargo-llvm-cov/releases/d
 mkdirSync('~/.local/bin', { recursive: true });
 const curl = spawn('curl', ['-sSL', CARGO_LLVM_COV_URL], { stdio: ['ignore', 'pipe', 'inherit'] });
 const tar = spawn('tar', ['zxf', '-', '-C', '~/.local/bin'], { stdio: [curl.stdout, 'inherit', 'inherit'] });
-tar.on('end', _ => {
-    const cargo = spawn('cargo', ['llvm-cov'], { env: { PATH: `~/.local/bin:${process.env['PATH']}` } });
-    cargo.on('end', console.log);
+tar.on('end', code => {
+    if (code) {
+        core.setFailed(`Failed to download cargo-llvm-cov: ${code}`);
+    }
+
+    const cargo = spawn(
+        'cargo',
+        ['llvm-cov'],
+        {
+            env: {
+                PATH: `~/.local/bin:${process.env['PATH']}`,
+            },
+            stdio: ['ignore', 'inherit', 'inherit']
+        });
+    cargo.on('end', code => {
+        if (code) {
+            core.setFailed(`Failed to run cargo llvm-cov: ${code}`);
+        }
+    });
 });
