d1d69975a42af2e3fada4bf1394c9c02a96e3a23
yskszk63 <yskszk63@gmail.com>
2021-04-11 00:50:57 +0900

    fail on error

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 CARGO_LLVM_COV_URL = 'https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.1.0-alpha.2/cargo-llvm-cov-x86_64-unknown-linux-musl.tar.gz'
 
 mkdir -p ~/.local/bin
