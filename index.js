e1870bdb0d4f42f3d8ac4c56792b2d9ed2b74342
yskszk63 <yskszk63@gmail.com>
2021-04-11 00:39:01 +0900

    use bash

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-const { spawn, spawnSync } = require('child_process');
-const { mkdirSync } = require('fs');
+const { exec } = require('@actions/exec');
 
-const CARGO_LLVM_COV_URL = 'https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.1.0-alpha.2/cargo-llvm-cov-x86_64-unknown-linux-musl.tar.gz';
-
-mkdirSync('~/.local/bin', { recursive: true });
-const curl = spawn('curl', ['-sSL', CARGO_LLVM_COV_URL], { stdio: ['ignore', 'pipe', 'inherit'] });
-const tar = spawnSync('tar', ['zxf', '-', '-C', '~/.local/bin'], { stdio: [curl.stdout, 'inherit', 'inherit'] });
-if (tar.code) {
-    core.setFailed(`Failed to download cargo-llvm-cov: ${code}`);
-}
-
-const cargo = spawnSync(
-    'cargo',
-    ['llvm-cov'],
-    {
-        env: {
-            PATH: `~/.local/bin:${process.env['PATH']}`,
-        },
-        stdio: ['ignore', 'inherit', 'inherit']
-    });
-if (cargo.code) {
-    core.setFailed(`Failed to run cargo llvm-cov: ${code}`);
-};
+exec('cargo', ['llvm-cov'], {
+    env: {
+        PATH: `~/.local/bin:${process.env['PATH']}`,
+    }
+});
---
+++ setup.js
@@ -0,0 +1,3 @@
+const { exec } = require('@actions/exec');
+
+await exec("./setup.sh");
